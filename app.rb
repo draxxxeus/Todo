6f1c02b9d9e56922882e892724edc1deec5738ad
Sudeep Agarwal <sudeep.ag@directi.com>
2015-02-02 00:27:53 +0530

    Remove used lines

--- app.rb
+++ app.rb
@@ -2,15 +2,12 @@ require 'sinatra'
 require 'sequel'
 
 class Todo < Sinatra::Application
-  set :environment, ENV['RACK_ENV']
-
   configure do
     DB = Sequel.connect("mysql://root:root@localhost/todo")
 
     Dir[File.join(File.dirname(__FILE__),'models','*.rb')].each { |model| require model }
   end
 
-  Dir[File.join(File.dirname(__FILE__),'lib','*.rb')].each { |lib| load lib }
 
 end
 
